1120851084b482d47851cb06502c67376628a441
thekillingjoke <chinmaynangia52@gmail.com>
2020-04-22 19:38:11 +0530

    Instagram Clone

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -1,62 +1,65 @@
 package com.example.instagram;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import  androidx.fragment.app.FragmentContainer;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-import com.example.instagram.Fragments.HomeFragment;
-import com.example.instagram.Fragments.NotificationFragment;
-import com.example.instagram.Fragments.ProfileFragment;
-import com.example.instagram.Fragments.SearchFragment;
-import com.example.instagram.Fragments.addFragment;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private BottomNavigationView bottomNavigationView;
-    private Fragment selectorFragment;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            bottomNavigationView = findViewById(R.id.bottom_navigation);
-            bottomNavigationView.setOnNavigationItemSelectedListener(
-                    new BottomNavigationView.OnNavigationItemSelectedListener() {
-                        @Override
-                        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                            switch (menuItem.getItemId()) {
-                                case R.id.nav_home:
-                                    selectorFragment = new HomeFragment();
-                                case R.id.nav_notification:
-                                    selectorFragment = new NotificationFragment();
-                                case R.id.nav_profile:
-                                    selectorFragment = new ProfileFragment();
-                                case R.id.nav_search:
-                                    selectorFragment = new SearchFragment();
-                                case R.id.add_fragment:
-                                    selectorFragment = new addFragment();
-                                    startActivity(new Intent(MainActivity.this,PostActivity.class));
-                                    break;
+    private     BottomNavigationView bottomNavigationView;
 
 
-                            }
-                            if (selectorFragment != null)
-                                Toast.makeText(MainActivity.this,"error a rha h ",Toast.LENGTH_LONG).show();
-                                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectorFragment).commit();
-                            return true;
-                        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState  ==  null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+        }
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment=null;
+                switch (menuItem.getItemId())
+                {
+                    case R.id.home :
+                        fragment=new HomeFragment();
+                        break;
 
-                    });
+                    case R.id.add :
+                        fragment=new AddFragment();
+                        startActivity(new Intent(MainActivity.this,PostActivity.class));
+                        break;
 
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectorFragment).commit();
+
+                    case R.id.search :
+                        fragment=new SearchFragment();
+                        break;
+                    case  R.id.profile :
+                        fragment=new ProfileFragment();
+                        break;
+                    case R.id.activity :
+                        fragment=new ActivityFragment();
+
+
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+                return true;
+            }
+        });
+
+
     }
 }
+
+
+
+
